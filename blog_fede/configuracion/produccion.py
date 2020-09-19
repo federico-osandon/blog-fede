4a9c96921277c90733e261621585215b84b01400
fedrico-osandon <48523760+fedrico-osandon@users.noreply.github.com>
2020-09-19 19:30:55 -0300

    Update produccion.py

--- blog_fede/configuracion/produccion.py
+++ blog_fede/configuracion/produccion.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['genplataforma.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
