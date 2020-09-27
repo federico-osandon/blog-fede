32d90015cdb2672d5099434093b3fd7b77eea8fc
fedrico-osandon <48523760+fedrico-osandon@users.noreply.github.com>
2020-09-27 03:19:46 -0300

    sdf

--- blog_fede/configuracion/produccion.py
+++ blog_fede/configuracion/produccion.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'genplataforma.pythonanywhere.com']
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
